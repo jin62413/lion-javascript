7ebe85afe5728b22d59cb9bddfcf47f290181445
jin62413 <jin62413@gmail.com>
2023-07-04 11:26:48 +0900

    variables 실습

---
+++ client/chapter/core/02.variables.js
@@ -0,0 +1,48 @@
+/* ------------------ */
+/* Variables          */
+/* ------------------ */
+
+let admin; // undefined
+let name; // undefined
+
+// 참조에 의한 복사
+
+name = 'John';
+admin = name;
+name = 'lion';
+
+/*console.log(admin); // john*/
+
+const ourPlanetName = 'Earth';
+const currentOnlineName;
+
+/* 다음 내용을 분석한 후, 프로그래밍 할 수 있도록 변수와 상수를 작성해봅니다. ----------- */
+
+// - 갯수 별 상품 가격 계산하기
+let calcProductPriceQuantity;
+
+// - 구매 제품 가격의 총 합
+let totalProductPrice;
+
+// - 1년 기준 일(day)자 수
+const DAYS_PER_YEAR = 365;
+
+// - 구매 결제 여부
+let isPayment = false;
+let hasClassName = true;
+
+// - 구매 결제 내역
+let paymentHistory;
+
+// - 브랜드 접두사
+const BRAND_PREFIX = 'NIKE';
+
+// - 오늘의 운세
+let fortuneOfToday;
+
+// - 상품 정보
+const productInfomation = '';
+
+/* variables ----------------------------------------------------------- */
+
+/* constant variables -------------------------------------------------- */
