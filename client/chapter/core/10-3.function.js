2ea8913127439f0ab65aee1c92ea59df8f8adec8
jin62413 <jin62413@gmail.com>
2023-07-11 10:45:26 +0900

    function 추가 학습

--- client/chapter/core/10-3.function.js
+++ client/chapter/core/10-3.function.js
@@ -50,6 +50,8 @@ const a = Button()
 const b = new Button() // b = {객체} */
 
 // 화살표 함수와 this
+// 일반 함수 : 나를 호출한 대상을 this로 바인딩합니다.
+// 화살표 함수 : this를 바인딩 하지 않음. 찾아야 한다면 내 부모꺼(상위 컨텍스트) 가져옴
 
 // 함수 선언문
 function nomalFunction() {
@@ -64,6 +66,38 @@ const expressionFunction = function () {
 // 화살표 함수식
 const arrowFunction = () => {
   console.log(this);
+};
+
+// 객체 안에서 this
+// 일반 함수: 나를 호출한 객체인 user가 불러와짐
+// 화살표 함수: 바인딩하지 않기 때문에 user의 부모인 window가 불러와짐(user엔 this가 없음)
+
+// 객체의 메서드를 정의할 때는 화살표 함수보다 일반 함수가 더 좋은거 아닌가요? yes
+// 매서드 안에서 함수를 호출할 때는 화살표 함수가 더 좋나? yes
+
+const user = {
+  total: 0,
+  name: 'tiger',
+  age: 32,
+  address: '서울시 중랑구 면목동',
+  grades: [80, 90, 100],
+  totalGrades: function () {
+    console.log(this.total);
+
+    this.grades.forEach((item) => {
+      this.total += item;
+    });
+
+    console.log(this.total);
+  },
+  // totalGrades: () => {
+  //   console.log(this.grades);
+  // },
+
+  // 일반함수 다른 표기법
+  // totalGrades() {
+  //   console.log(this.grades);
+  // },
 };
 
 /* 다음 함수를 작성해봅니다. -------------------------------------------------- */
