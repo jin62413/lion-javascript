02a1246934855b1b7c1a66d57a1ce702915dece5
jin62413 <jin62413@gmail.com>
2023-07-20 16:42:05 +0900

    주석 추가

--- client/lib/dom/clearContents.js
+++ client/lib/dom/clearContents.js
@@ -4,10 +4,12 @@ import { getNode } from "./getNode.js";
 export function clearContents(node){
 
   if(typeof node === 'string') node = getNode(node);
+  // input, textArea는 nodeName으로 내용을 인식
   if(node.nodeName === 'INPUT' || node.nodeName === 'TEXTAREA'){
     node.value = ''
     return;
   }
 
+  // 다른 요소들은 textContent를 사용하면 내용 인식
   node.textContent = ''
 }
